b858c24461d18fd03044440d2971f3ad88eab644
Sulfrix <deucebroom@outlook.com>
2023-04-18 22:46:19 -0500

    stuff

--- scripts/downloadProcessing-linux-x64.sh
+++ scripts/downloadProcessing-linux-x64.sh
@@ -11,14 +11,14 @@ cd temp
 rm -rf ./processing-4.0b7
 
 # fixed download URL
-#wget -O temp.tgz https://github.com/processing/processing4/releases/download/processing-1282-4.0b7/processing-4.0b7-linux-x64.tgz
+wget -O temp.tgz https://github.com/processing/processing4/releases/download/processing-1292-4.2/processing-4.2-linux-x64.tgz
 
 # extract
 tar -xf temp.tgz
 
 # move
 mkdir -p ../../processing
-mv ./processing-4.0b7/* ../../processing
+mv ./processing-4.2/* ../../processing
 
-rm -rf processing-4.0b7
+rm -rf processing-4.2
 
